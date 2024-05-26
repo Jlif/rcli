6f7f3a0c78e83d74c9ca0efc22a7861aa7fe22b3
蒋琛 <jiangchen@192.168.88.100>
2024-05-27 00:40:25 +0800

    feature: add base64 surpport

--- src/process/mod.rs
+++ src/process/mod.rs
@@ -1,5 +1,7 @@
-mod csv_converts;
+mod b64;
+mod csv_convert;
 mod gen_pass;
 
-pub use csv_converts::process_csv;
+pub use b64::{process_decode, process_encode};
+pub use csv_convert::process_csv;
 pub use gen_pass::process_genpass;
